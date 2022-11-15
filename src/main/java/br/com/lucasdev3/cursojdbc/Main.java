99ca291bfbe2ffed3d70e9fc4d0bdc2fa3d89901
lucasdev3 <lucasrepositoriodados@gmail.com>
2022-11-15 14:07:41 -0300

    update logic persistence

--- src/main/java/br/com/lucasdev3/cursojdbc/Main.java
+++ src/main/java/br/com/lucasdev3/cursojdbc/Main.java
@@ -1,38 +1,49 @@
 package br.com.lucasdev3.cursojdbc;
 
+import br.com.lucasdev3.cursojdbc.entities.User;
+
 import static br.com.lucasdev3.cursojdbc.services.UserService.persistUser;
 import static br.com.lucasdev3.cursojdbc.services.UserService.findAll;
 import static br.com.lucasdev3.cursojdbc.services.UserService.findById;
 import static br.com.lucasdev3.cursojdbc.services.UserService.findByUsername;
 
-import java.sql.SQLException;
+import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String username = "";
         String password = "";
 
         //CADASTRO DE USUARIO
-//        try {
-//            System.out.println("Informe o username: ");
-//            username = sc.nextLine();
-//            System.out.println("Informe a senha: ");
-//            password = sc.nextLine();
-//            persistUser(username, password);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+        try {
+            System.out.println("Informe o username: ");
+            username = sc.nextLine();
+            System.out.println("Informe a senha: ");
+            password = sc.nextLine();
+            persistUser(username, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         // CONSULTAS
         try {
             System.out.println("Find All");
-            findAll();
-            System.out.println("\nFind By Id");
-            findById(6);
-            System.out.println("\nFind By Username");
-            findByUsername("TESTE2");
+            List<User> listUser =  findAll();
+            if(Objects.requireNonNull(listUser).size() > 0) {
+                for(User user : listUser) {
+                    System.out.println("ID: " + user.getId() + " | Username: " + user.getUsername() + " | Password: " + user.getPassword());
+                }
+            } else {
+                System.out.println("Nenhum usuario encontrado! Método findAll");
+            }
+
+//            System.out.println("\nFind By Id");
+//            findById(6);
+//            System.out.println("\nFind By Username");
+//            User user = findByUsername("TESTE2");
         }catch (Exception e) {
             e.printStackTrace();
         }
--- src/main/java/br/com/lucasdev3/cursojdbc/services/UserService.java
+++ src/main/java/br/com/lucasdev3/cursojdbc/services/UserService.java
@@ -6,13 +6,14 @@ import br.com.lucasdev3.cursojdbc.entities.User;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public abstract class UserService {
 
-    public static void findById(Integer id) {
+    public static User findById(Integer id) {
         try {
             Connection connection = ConnectionDataBase.getConnection();
-            System.out.println("Conexão Realizada com sucesso!");
+//            System.out.println("Conexão Realizada com sucesso!");
 
             String sql = "SELECT * FROM TB_USERS WHERE id = ?";
             PreparedStatement ps = connection.prepareStatement(sql);
@@ -24,19 +25,21 @@ public abstract class UserService {
                 user.setId(rs.getInt("id"));
                 user.setUsername(rs.getString("username"));
                 user.setPassword(rs.getString("password"));
-                System.out.println("ID: " + user.getId() + " | Username: " + user.getUsername() + " | Password: " + user.getPassword());
+//                System.out.println("ID: " + user.getId() + " | Username: " + user.getUsername() + " | Password: " + user.getPassword());
+                return user;
             } else {
                 System.out.println("Nenhum usuario encontrado");
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return null;
     }
 
-    public static void findByUsername(String username) {
+    public static User findByUsername(String username) {
         try {
             Connection connection = ConnectionDataBase.getConnection();
-            System.out.println("Conexão Realizada com sucesso!");
+//            System.out.println("Conexão Realizada com sucesso!");
 
             String sql = "SELECT * FROM TB_USERS WHERE username = ?";
             PreparedStatement ps = connection.prepareStatement(sql);
@@ -48,19 +51,21 @@ public abstract class UserService {
                 user.setId(rs.getInt("id"));
                 user.setUsername(rs.getString("username"));
                 user.setPassword(rs.getString("password"));
-                System.out.println("ID: " + user.getId() + " | Username: " + user.getUsername() + " | Password: " + user.getPassword());
+//                System.out.println("ID: " + user.getId() + " | Username: " + user.getUsername() + " | Password: " + user.getPassword());
+                return user;
             } else {
                 System.out.println("Nenhum usuario encontrado");
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return null;
     }
 
     public static List<User> findAll() {
         try {
             Connection connection = ConnectionDataBase.getConnection();
-            System.out.println("Conexão Realizada com sucesso!");
+//            System.out.println("Conexão Realizada com sucesso!");
 
             String sql = "SELECT * FROM TB_USERS";
 
@@ -75,12 +80,9 @@ public abstract class UserService {
                 String password = rs.getString("password");
                 listUsers.add(new User(id, username, password));
             }
-
-            for (User user : listUsers) {
-                System.out.println("ID: " + user.getId() + " | Username: " + user.getUsername() + " | Password: " + user.getPassword());
-            }
             connection.close();
             System.out.println("Conexão encerrada!");
+            return listUsers;
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -88,22 +90,33 @@ public abstract class UserService {
     }
 
     public static void persistUser(String username, String password) {
-        try {
-            Connection connection = ConnectionDataBase.getConnection();
-            System.out.println("Conexão Realizada com sucesso!");
+        username = username.trim();
+        password = password.trim();
+        if (!username.isEmpty() && !password.isEmpty()) {
+            try {
+                Connection connection = ConnectionDataBase.getConnection();
+//            System.out.println("Conexão Realizada com sucesso!");
 
-            String sql = "INSERT INTO TB_USERS (username, password) VALUES (?, ?)";
+                User userFound = findByUsername(username);
+                if (Objects.isNull(userFound)) {
+                    String sql = "INSERT INTO TB_USERS (username, password) VALUES (?, ?)";
 
-            PreparedStatement ps = connection.prepareStatement(sql);
-            ps.setString(1, username);
-            ps.setString(2, password);
+                    PreparedStatement ps = connection.prepareStatement(sql);
+                    ps.setString(1, username);
+                    ps.setString(2, password);
 
-            ps.execute();
-            System.out.println("Usuario cadastrado com sucesso!");
-            connection.close();
-            System.out.println("Conexão encerrada!");
-        } catch (SQLException e) {
-            e.printStackTrace();
+                    ps.execute();
+                    System.out.println("Usuario cadastrado com sucesso!");
+                    connection.close();
+//                System.out.println("Conexão encerrada!");
+                } else {
+                    System.out.println("Usuário já cadastrado");
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Usuário ou senha inválidos");
         }
     }
 }
